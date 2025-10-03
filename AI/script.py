e5111eec16da27e3a6fdac52780dad0e8217a6b3
maybe0hiren <hirenwaghmare25@gmail.com>
2025-10-03 14:45:59 +0530

    Flask api for OCR

--- AI/script.py
+++ AI/script.py
@@ -1,33 +1,46 @@
+from flask import Flask, request, jsonify
+import requests
+
 from google import genai
 from PIL import Image
 import json
 
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
 client = genai.Client(api_key=api_key)
 
-def extract(image_path):
+app = Flask(__name__)
+
+@app.route('/process-image', methods=['POST'])
+def extract():
+    if 'image' not in request.files:
+        return jsonify({
+            "error: Image not found"
+        })
+    image = request.files['image']
+    image.save(os.path.join("tempStorage", "image.jpeg"))
+    
+    image_path = os.path.join("tempStorage", "image.jpeg")
     uploaded_file = client.files.upload(file=image_path)
     prompt = (
-        "Extract the name of the receiver and amount paid from the following image. Just give me 2 things, name and amount without the rupee sign, separated by comma nothing else."
+        "Extract the name of the receiver, date of transaction and amount paid from the following image. Just give me 3 things, name, date (exclude the time too) and amount without the rupee sign, separated by comma nothing else. If the image is not valid, return INVALID text for all 3 parameters"
     )
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=[uploaded_file, prompt]
     )
     full_text = response.text
-    parts = full_text.split(",", 1)
+    parts = full_text.split(",", 2)
     array = [part.strip() for part in parts]
     print(array) 
-    jsonData = {
+    return jsonify({
         "receiver": array[0],
-        "amount": array[1]
-    }
-    with open("payment.json", "w") as json_file:
-        json.dump(jsonData, json_file, indent=4)
+        "date": array[1],
+        "amount": float (array[2])
+    })
 
-extract("image.jpeg")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
